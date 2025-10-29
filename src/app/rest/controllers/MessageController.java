6dab80acbcb419da4a647c192b45a0afdbb47960
iel2408 <161427873+iel2408@users.noreply.github.com>
2025-10-29 13:37:06 +0800

    Update MessageController.java

--- src/app/rest/controllers/MessageController.java
+++ src/app/rest/controllers/MessageController.java
@@ -16,64 +16,31 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import app.component.StrayAnimalComponent;
-import app.component.SightingComponent;
-import app.entities.Sighting;
-import app.entities.StrayAnimal;
+import app.components.Message;
+import app.entities.Quote;
 
 
-@Component
-@Path("/strays")
-public class StrayAnimalController {
+@Path("/messageServer")
+public class MessageController {
 	
-	
-	Logger logger = LoggerFactory.getLogger(StrayAnimalController.class);
-		
+	Logger logger = LoggerFactory.getLogger(MessageController.class);
 	
 	@Autowired
-	private StrayAnimalComponent strayAnimal;
-	
-	@Autowired
-	private SightingComponent sight;
-	
+	private Message message;
 	
 	@POST
-    @Path("/add")
-	@Produces(MediaType.TEXT_PLAIN)
-	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-	public Long addStray(
+ @Path("/getQuote")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	
+	// GAWING DTO
+	public Quote getQuote(
 	        @FormParam("type") String type,
 	        @FormParam("color") String color,
 	        @FormParam("neutered") Boolean neutered,
 	        @FormParam("description") String description) 
 	{
-		return strayAnimal.saveStray(type, color, neutered, description);
-	}
-	
-	//REMOVE
-	@POST
-	@Path("/sighting/new")
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	public Sighting addSighting(
-			SightingDto parameterObject) 
-	{
-		Sighting s = sight.addSighting(parameterObject.getLocation(), parameterObject.getLatitude(), parameterObject.getLongitude(), parameterObject.getType(), parameterObject.getColor(), parameterObject.isNeutered(), parameterObject.getAnimalDescription(), parameterObject.getComment());
-		return s;
-	}
-	
-	@GET
-	@Path("/bytype/{type}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public List<StrayAnimal> getByType(@PathParam("type") String type) {
-		return strayAnimal.getByType(type);
-	}
-
-	@GET
-	@Path("/neutered")
-	@Produces(MediaType.APPLICATION_JSON)
-	public List<StrayAnimal> getNotNeutered() {
-		return strayAnimal.getNotNeutered();
+		
 	}
 }
 	
